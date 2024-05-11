2ee7f598126eff230138923e5a3603e4273820c1
Salieri-Kuris <1065254539@qq.com>
2024-05-11 17:47:40 +0800

    something wrong....

--- dmrg_lieb.jl
+++ dmrg_lieb.jl
@@ -2,6 +2,7 @@ using Plots
 using ITensors
 using Base.Threads
 using LinearAlgebra
+using Formatting
 Threads.nthreads()
 include("my_lattice.jl")
 # 关闭BLAS和Strided多线程
@@ -9,9 +10,16 @@ BLAS.set_num_threads(1)
 ITensors.Strided.disable_threads()
 # 根据需要开启块稀疏多线程
 ITensors.enable_threaded_blocksparse(true)
-function main(; Nx=6, Ny=4, U=4.0, t=1.0, tp=0)
-    N = 2Nx * 2Ny
-    nsweeps = 10
+Nx = 6 
+Ny = 4
+U = 100.0
+del = 0.0
+tp = 0.0
+
+function main(; Nx, Ny, U, del, tp)
+    N = 3 * Nx * Ny
+    t = 1.0
+    nsweeps = 20
     maxdim = [100, 200, 400, 800, 1600]
     cutoff = [1E-6]
     noise = [1E-6, 1E-7, 1E-8, 0.0]
@@ -21,8 +29,9 @@ function main(; Nx=6, Ny=4, U=4.0, t=1.0, tp=0)
     lattice1 = lieb_lattice_intra(Nx, Ny; yperiodic=true)
     lattice2 = lieb_lattice_inter(Nx, Ny; yperiodic=true)
     lattice3 = lieb_lattice_3NN(Nx, Ny; yperiodic=true)
-    lattice4 = lieb_lattice(Nx, Ny; yperiodic=true)
     os = OpSum()
+    t1 =  t * (1 + del)
+    t2 =  t * (1 - del)
     for b in lattice1
         os += -t, "Cdagup", b.s1, "Cup", b.s2
         os += -t, "Cdagup", b.s2, "Cup", b.s1
@@ -35,24 +44,24 @@ function main(; Nx=6, Ny=4, U=4.0, t=1.0, tp=0)
         os += -t, "Cdagdn", b.s1, "Cdn", b.s2
         os += -t, "Cdagdn", b.s2, "Cdn", b.s1
     end
-    for b in lattice3
-        os += -tp, "Cdagup", b.s1, "Cup", b.s2
-        os += -tp, "Cdagup", b.s2, "Cup", b.s1
-        os += -tp, "Cdagdn", b.s1, "Cdn", b.s2
-        os += -tp, "Cdagdn", b.s2, "Cdn", b.s1
-    end
+    # for b in lattice3
+    #     os += -tp, "Cdagup", b.s1, "Cup", b.s2
+    #     os += -tp, "Cdagup", b.s2, "Cup", b.s1
+    #     os += -tp, "Cdagdn", b.s1, "Cdn", b.s2
+    #     os += -tp, "Cdagdn", b.s2, "Cdn", b.s1
+    # end
     for n in 1:N
         os += U, "Nupdn", n
     end
     H = MPO(os, sites)
     # Half filling
     # state = [(mod(n,4)==1 || mod(n,4) == 2) ? "Up" : "Dn" for n in 1:N]
-    state = [isodd(n) ? "Dn" : "Up" for n in 1:N]
+    state = [mod(n, 3)>1 ? "Dn" : "Up" for n in 1:N]
 
     # Initialize wavefunction to a random MPS
     # of bond-dimension 10 with same quantum
     # numbers as `state`
-    psi0 = randomMPS(sites, state; linkdims=3)
+    psi0 = randomMPS(sites, state; linkdims=5)
 
     energy, psi = dmrg(H, psi0; nsweeps, maxdim, cutoff, noise)
     @show t, U
@@ -63,7 +72,33 @@ function main(; Nx=6, Ny=4, U=4.0, t=1.0, tp=0)
 end
 # # 如果需要，在计算完成后恢复默认设置
 # ITensors.enable_threaded_blocksparse(false)
-psi = main()
+psi = main(;Nx, Ny, U, del, tp)
 magz = expect(psi, "Sz")
 sum(magz)
-plot_lieb_lattice_magnetic_moments(6,4,magz)
+plot_lieb_lattice_magnetic_moments(Nx, Ny, U, del, tp, magz)
+anti_order(magz, 4)
+lattice1 = lieb_lattice_intra(Nx, Ny; yperiodic=true)
+lattice2 = lieb_lattice_inter(Nx, Ny; )
+plot_bond(lattice1)
+t = 1.0
+os = OpSum()
+    t1 =  t * (1 + del)
+    t2 =  t * (1 - del)
+    for b in lattice1
+        os += -t1, "Cdagup", b.s1, "Cup", b.s2
+        os += -t1, "Cdagup", b.s2, "Cup", b.s1
+        os += -t1, "Cdagdn", b.s1, "Cdn", b.s2
+        os += -t1, "Cdagdn", b.s2, "Cdn", b.s1
+    end
+    for b in lattice2
+        os += -t2, "Cdagup", b.s1, "Cup", b.s2
+        os += -t2, "Cdagup", b.s2, "Cup", b.s1
+        os += -t2, "Cdagdn", b.s1, "Cdn", b.s2
+        os += -t2, "Cdagdn", b.s2, "Cdn", b.s1
+    end
+os
+sites = siteinds("Electron", 72; conserve_qns=true)
+state = [mod(n, 3)>1 ? "Dn" : "Up" for n in 1:72]
+psi0 = randomMPS(sites, state; linkdims=5)
+mag_random = expect(psi0, "Sz")
+sum(mag_random)
--- my_lattice.jl
+++ my_lattice.jl
@@ -1,4 +1,5 @@
 using ITensors
+using Formatting
 function plot_bond(lattice::Lattice)
   # Create the plot
   plt = plot(size=(600,600), axis_buffer=0.1, reuse=false)
@@ -7,12 +8,18 @@ function plot_bond(lattice::Lattice)
   for bond in lattice
       plot!(plt, [bond.x1, bond.x2], [bond.y1, bond.y2], color=:black, label="")
   end
-
+  sites1 = unique([bond.x1, bond.y1] for bond in lattice)
+  sites2 = unique([bond.x2, bond.y2] for bond in lattice)
+  sites = unique(cat(sites1, sites2, dims=1))
+  siteA = [site for site in sites if mod(site[1], 2) == 1 && mod(site[2], 2) == 1]
+  siteB = setdiff(sites, siteA)
   # Plot sites as points
-  scatter!(plt, [bond.x1 for bond in lattice], [bond.y1 for bond in lattice], 
+
+  scatter!(plt, [site[1] for site in siteA], [site[2] for site in siteA], 
            label="", color=:blue, markersize=5)
-  scatter!(plt, [bond.x2 for bond in lattice], [bond.y2 for bond in lattice], 
-          label="", color=:blue, markersize=5)
+  scatter!(plt, [site[1] for site in siteB], [site[2] for site in siteB], 
+           label="", color=:red, markersize=5)
+
 
   # Configure the plot
   plot!(plt, xlabel="x", ylabel="y", title="Lieb Lattice")
@@ -20,54 +27,6 @@ function plot_bond(lattice::Lattice)
   display(plt) # Display the plot
 end
 
-function lieb_lattice(Nx::Int, Ny::Int; yperiodic=false)::Lattice
-  yperiodic = yperiodic && (Ny > 1)
-  N = 2Nx * 2Ny
-  Nbond = 2 * Nx * Ny + 2 * Nx * Ny - Ny + (yperiodic ? 0 : -Nx)
-  latt = Lattice(undef, Nbond)
-  b = 0
-  for n in 1:N
-    x = div(n - 1, 2Ny) + 1
-    y = mod(n - 1, 2Ny) + 1
-    if mod(x, 2) == 1 && mod(y, 2) == 1
-      latt[b += 1] = LatticeBond(n, n + 2Ny, x, y, x + 1, y)
-      latt[b += 1] = LatticeBond(n, n + 1, x, y, x, y + 1)
-    end
-  end
-  for n in 1:N
-    x = div(n - 1, 2Ny) + 1
-    y = mod(n - 1, 2Ny) + 1
-    if x > 1 && mod(x, 2) == 1 && mod(y, 2) == 1
-      latt[b += 1] = LatticeBond(n, n - 2Ny, x, y, x - 1, y)
-    end
-    if Ny > 1
-      if y >1 && mod(x, 2) == 1 && mod(y, 2) == 1
-        latt[b += 1] = LatticeBond(n, n - 1, x, y, x, y - 1)
-      end
-      if yperiodic && y == 1 && mod(x, 2) == 1
-        latt[b += 1] = LatticeBond(n, n + 2Ny - 1, x, y, x, y + 2Ny - 1)
-      end
-    end
-  end
-  return latt
-end
-# function lieb_lattice_intra(Nx::Int, Ny::Int; yperiodic=false)::Lattice
-#   yperiodic = yperiodic && (Ny > 2)
-#   N = 2Nx * 2Ny
-#   Nbond = 2 * Nx * Ny
-#   latt = Lattice(undef, Nbond)
-#   b = 0
-#   for n in 1:N
-#     x = div(n - 1, 2Ny) + 1
-#     y = mod(n - 1, 2Ny) + 1
-#     if mod(x, 2) == 1 && mod(y, 2) == 1
-#       latt[b += 1] = LatticeBond(n, n + 2Ny, x, y, x + 1, y)
-#       latt[b += 1] = LatticeBond(n, n + 1, x, y, x, y + 1)
-#     end
-#   end
-#     return latt
-  
-# end
 function lieb_lattice_intra(Nx::Int, Ny::Int; yperiodic=false)::Lattice
   yperiodic = yperiodic && (Ny > 2)
   N = 3 * Nx * Ny
@@ -78,7 +37,7 @@ function lieb_lattice_intra(Nx::Int, Ny::Int; yperiodic=false)::Lattice
     x = 2 * div(n, 3Ny) + ((mod(n , 3Ny) != 0) ? (mod(n, 3Ny) > 2Ny ? 2 : 1) : 0)
     y = (mod(n, 3Ny) > 2Ny  ? 2(mod(n, 3Ny) - 2Ny)-1 : (mod(n, 3Ny) == 0 ? 2Ny-1 : mod(n, 3Ny)))
     if mod(x, 2) == 1 && mod(y, 2) == 1
-      latt[b += 1] = LatticeBond(n, n + 2Ny, x, y, x + 1, y)
+      latt[b += 1] = LatticeBond(n, n + 2Ny - Int((y - 1)/2), x, y, x + 1, y)
       latt[b += 1] = LatticeBond(n, n + 1, x, y, x, y + 1)
     end
   end
@@ -86,29 +45,6 @@ function lieb_lattice_intra(Nx::Int, Ny::Int; yperiodic=false)::Lattice
   
 end
 
-# function lieb_lattice_inter(Nx::Int, Ny::Int; yperiodic=false)::Lattice
-#     yperiodic = yperiodic && (Ny > 2)
-#     N = 2Nx * 2Ny
-#     Nbond = 2 * Nx * Ny - Ny + (yperiodic ? 0 : -Nx)
-#     latt = Lattice(undef, Nbond)
-#     b = 0
-#     for n in 1:N
-#       x = div(n - 1, 2Ny) + 1
-#       y = mod(n - 1, 2Ny) + 1
-#       if x > 1 && mod(x, 2) == 1 && mod(y, 2) == 1
-#         latt[b += 1] = LatticeBond(n, n - 2Ny, x, y, x - 1, y)
-#       end
-#       if Ny > 1
-#         if y >1 && mod(x, 2) == 1 && mod(y, 2) == 1
-#           latt[b += 1] = LatticeBond(n, n - 1, x, y, x, y - 1)
-#         end
-#         if yperiodic && y == 1 && mod(x, 2) == 1
-#           latt[b += 1] = LatticeBond(n, n + 2Ny - 1, x, y, x, y + 2Ny - 1)
-#         end
-#       end
-#     end
-#     return latt
-#   end
 
 function lieb_lattice_inter(Nx::Int, Ny::Int; yperiodic=false)::Lattice
   yperiodic = yperiodic && (Ny > 2)
@@ -120,7 +56,7 @@ function lieb_lattice_inter(Nx::Int, Ny::Int; yperiodic=false)::Lattice
     x = 2 * div(n, 3Ny) + ((mod(n , 3Ny) != 0) ? (mod(n, 3Ny) > 2Ny ? 2 : 1) : 0)
     y = (mod(n, 3Ny) > 2Ny  ? 2(mod(n, 3Ny) - 2Ny)-1 : (mod(n, 3Ny) == 0 ? 2Ny-1 : mod(n, 3Ny)))
     if x > 1 && mod(x, 2) == 1 && mod(y, 2) == 1
-      latt[b += 1] = LatticeBond(n, n - 2Ny, x, y, x - 1, y)
+      latt[b += 1] = LatticeBond(n, n - Ny - Int((y - 1)/2), x, y, x - 1, y)
     end
     if Ny > 1
       if y >1 && mod(x, 2) == 1 && mod(y, 2) == 1
@@ -134,42 +70,47 @@ function lieb_lattice_inter(Nx::Int, Ny::Int; yperiodic=false)::Lattice
     return latt
   
 end
+
+
 function lieb_lattice_3NN(Nx::Int, Ny::Int; yperiodic=false)::Lattice
   yperiodic = yperiodic && (Ny > 2)
-  N = 2Nx * 2Ny
+  N = 3 * Nx * Ny
   Nbond = 2 * 3 * Nx * Ny - 3Ny + (yperiodic ? 0 : -3Nx)
   latt = Lattice(undef, Nbond)
   b = 0
   for n in 1:N
-    x = div(n - 1, 2Ny) + 1
-    y = mod(n - 1, 2Ny) + 1
-    if x < 2Nx - 1 && (mod(y, 2) == 1 || mod(x, 2) == 1)
-      latt[b += 1] = LatticeBond(n, n + 4Ny, x, y, x + 2, y)
+    x = 2 * div(n, 3Ny) + ((mod(n , 3Ny) != 0) ? (mod(n, 3Ny) > 2Ny ? 2 : 1) : 0)
+    y = (mod(n, 3Ny) > 2Ny  ? 2(mod(n, 3Ny) - 2Ny)-1 : (mod(n, 3Ny) == 0 ? 2Ny-1 : mod(n, 3Ny)))
+    if x < 2Nx - 1 
+      latt[b += 1] = LatticeBond(n, n + 3Ny, x, y, x + 2, y)
     end
     if Ny > 1
-      if y < 2Ny - 1 && (mod(y, 2) == 1 || mod(x, 2) == 1)
+      if y < 2Ny - 1 && mod(x, 2) == 1 
         latt[b += 1] = LatticeBond(n, n + 2, x, y, x, y + 2)
+      elseif y < 2Ny - 1 && mod(x, 2) == 0
+        latt[b += 1] = LatticeBond(n, n + 1, x, y, x, y + 2)
       end
-      if yperiodic && y == 2Ny - 1 && (mod(y, 2) == 1 || mod(x, 2) == 1)
+      if yperiodic && y == 2Ny - 1 && mod(x, 2) == 1
         latt[b += 1] = LatticeBond(n, n - 2Ny + 2, x, y, x, y - 2Ny + 2)
+      elseif yperiodic && y == 2Ny - 1 && mod(x, 2) == 0
+        latt[b += 1] = LatticeBond(n, n - Ny + 1, x, y, x, y - 2Ny + 2)
       end
-      if yperiodic && y == 2Ny && (mod(y, 2) == 1 || mod(x, 2) == 1)
+      if yperiodic && y == 2Ny 
         latt[b += 1] = LatticeBond(n, n - 2Ny + 2, x, y, x, y - 2Ny + 2)
       end
     end
   end
-  return latt
+    return latt
 end
 
-function plot_lieb_lattice_magnetic_moments(Nx, Ny, magz)
-  @assert length(magz) == 4 * Nx * Ny "The length of magnetic moments vector must equal 4 * Nx * Ny."
+function plot_lieb_lattice_magnetic_moments(Nx, Ny, U, delta, tp, magz)
+  @assert length(magz) == 3 * Nx * Ny "The length of magnetic moments vector must equal 4 * Nx * Ny."
   plot_size = max(Nx, Ny) * 100
-  # Create a scatter plot for the magnetic moments
   plt = scatter(
       # x coordinates for each site
-      [div(n - 1, 2 * Ny) + 1 for n in 1:length(magz)],
+      [2 * div(n, 3Ny) + ((mod(n , 3Ny) != 0) ? (mod(n, 3Ny) > 2Ny ? 2 : 1) : 0) for n in 1:length(magz)],
       # y coordinates for each site
-      [mod(n - 1, 2 * Ny) + 1 for n in 1:length(magz)],
+      [(mod(n, 3Ny) > 2Ny  ? 2(mod(n, 3Ny) - 2Ny)-1 : (mod(n, 3Ny) == 0 ? 2Ny-1 : mod(n, 3Ny))) for n in 1:length(magz)],
       # Magnetic moment values as annotations
       series_annotations=[(string(round(val, digits=4)), font(8,colorant"red")) for val in magz],
       # Markersize is set to a small value to make sure the annotations are visible
@@ -178,8 +119,7 @@ function plot_lieb_lattice_magnetic_moments(Nx, Ny, magz)
       # Set the aspect ratio to 1 for a square lattice
       aspect_ratio=0.6,
       # Optionally, set a title for the plot
-      title="Magnetic Moments on Square Lattice",
-      # Set the x and y limits to match the lattice size
+      title = "Magnetic Moments on Lieb Lattice, U = $(U), delta = $(delta), tp = $(tp)",      # Set the x and y limits to match the lattice size
       xlims=(1, 2 * Nx),
       ylims=(1, 2 * Ny),
       # Other styling options
@@ -194,5 +134,18 @@ function plot_lieb_lattice_magnetic_moments(Nx, Ny, magz)
   # Display the plot
   display(plt)
   return plt
+end
+
+function anti_order(magz, Ny)
+  N = length(magz)
+  order = 0
+  for n in 1:N
+    x = 2 * div(n, 3Ny) + ((mod(n , 3Ny) != 0) ? (mod(n, 3Ny) > 2Ny ? 2 : 1) : 0)
+    y = (mod(n, 3Ny) > 2Ny  ? 2(mod(n, 3Ny) - 2Ny)-1 : (mod(n, 3Ny) == 0 ? 2Ny-1 : mod(n, 3Ny)))
+    if mod(x, 2) == 1 && mod(y, 2) == 1
+      order += (magz[n] + magz[n+1] + magz[n + 2Ny - Int((y - 1)/2)]) * (-1)^((mod(n, 3Ny)-1)/2 )
+    end
+  end
+  return order
 end
 
